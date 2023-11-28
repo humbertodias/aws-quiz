333206444bfbf731ea462ac129029f4d9b11b26a
Humberto Dias <hldtux@gmail.com>
2023-11-28 00:05:42 -0300

    replacing artyom for SpeechSynthesisUtterance

--- src/speak.tsx
+++ src/speak.tsx
@@ -1,44 +1,12 @@
-// // Import the Artyom library
-// import Artyom from "artyom.js"
-
-// // Create a "globally" accesible instance of Artyom
-// export const Jarvis = new Artyom();
-
+let msg: SpeechSynthesisUtterance;
 export const startAssistant = () => {
-  // console.log("Artyom succesfully started !");
-
-  // Jarvis.initialize({
-  //   lang: "en-US",
-  //   debug: true,
-  //   continuous: true,
-  //   soundex: true,
-  //   listen: true,
-  // })
-  //   .then(() => {
-  //     // Display loaded commands in the console
-  //     console.log(Jarvis.getAvailableCommands());
-  //   })
-  //   .catch((err: string) => {
-  //     console.error("Oopsy daisy, this shouldn't happen !", err);
-  //   });
+  msg = new SpeechSynthesisUtterance();
 };
 
-export const stopAssistant = () => {
-  // Jarvis.fatality()
-  //   .then(() => {
-  //     console.log("Jarvis has been succesfully stopped");
-  //   })
-  //   .catch((err: string) => {
-  //     console.error("Oopsy daisy, this shouldn't happen neither!", err);
-  //   });
-};
+export const stopAssistant = () => {};
 
 export const speakText = (text: string) => {
   console.log(text);
-  // // Speak text with Artyom
-  // Jarvis.say(text, {
-  //   onEnd() {
-  //     console.log("No more text to talk");
-  //   },
-  // });
+  msg.text = text;
+  window.speechSynthesis.speak(msg);
 };
