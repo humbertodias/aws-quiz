a6cbadd74b0bed6c94dc3082372a4ca12f9faf99
Humberto Dias <hldtux@gmail.com>
2023-12-02 18:01:27 -0300

    Icons

--- src/components/ExamCard.tsx
+++ src/components/ExamCard.tsx
@@ -2,11 +2,10 @@ import { Link } from "react-router-dom";
 import ExamProps from "../type/ExamProps";
 
 import AssignmentIcon from "@mui/icons-material/Assignment";
-import GpsFixedIcon from '@mui/icons-material/GpsFixed';
-import ContentPasteIcon from '@mui/icons-material/ContentPaste';
-import BadgeIcon from '@mui/icons-material/Badge';
-import MoreVertIcon from '@mui/icons-material/Badge';
-import ExpandMoreIcon from '@mui/icons-material/Badge';
+import GpsFixedIcon from "@mui/icons-material/GpsFixed";
+import ContentPasteIcon from "@mui/icons-material/ContentPaste";
+import BadgeIcon from "@mui/icons-material/Badge";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import {
   Card,
@@ -24,8 +23,6 @@ import {
 
 import { useState } from "react";
 
-
-
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
 }
@@ -34,16 +31,14 @@ const ExpandMore = styled((props: ExpandMoreProps) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
 })(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
     duration: theme.transitions.duration.shortest,
   }),
 }));
 
 function ExamCard(exam: ExamProps) {
-
-
   const [expanded, setExpanded] = useState(false);
 
   const handleExpandClick = () => {
@@ -52,90 +47,73 @@ function ExamCard(exam: ExamProps) {
 
   return (
     <div key={exam.id}>
-
-
-
-<Card sx={{ maxWidth: 345, margin: 1 }}>
-      <CardHeader
-        // avatar={
-        //   <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-        //     R
-        //   </Avatar>
-        // }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={exam.id}
-        // subheader="September 14, 2016"
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image={exam.image}
-        alt="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-        {exam.description.substring(0,100)}...
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-
-      <Link
-              target={"_blank"}
-              to={exam.guide}
-            >
-              <Button  title="Guide">
-                <GpsFixedIcon />
-                {/* Guide */}
-              </Button>
-            </Link>
-            <Link
-              target={"_blank"}
-              to={exam.sample}
-            >
-              <Button  title="Sample">
-                <ContentPasteIcon />
-                {/* Sample */}
-              </Button>
-            </Link>
-            <Link
-              target={"_blank"}
-              to={exam.practice}
-            >
-              <Button  title="Practice">
-                <BadgeIcon />
-                {/* Practice */}
-              </Button>
-            </Link>
+      <Card sx={{ maxWidth: 345, margin: 1 }}>
+        <CardHeader
+          // avatar={
+          //   <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+          //     R
+          //   </Avatar>
+          // }
+          action={
             <Link to={`/exam/${exam.id}`}>
-              <Button  title="Exam">
+              <Button title="Exam">
                 <AssignmentIcon />
-                {/* Exam */}
+                Exam
               </Button>
             </Link>
-
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
+          }
+          title={exam.id}
+          // subheader="September 14, 2016"
+        />
+        <Link to={`/exam/${exam.id}`}>
+        <CardMedia className="hover:scale-110 transition duration-500 cursor-pointer"
+          component="img"
+          height="194"
+          image={exam.image}
+          alt="Paella dish"
+        />
+        </Link>
         <CardContent>
-          <Typography paragraph>Description:</Typography>
-          <Typography paragraph>
-          {exam.description}
+          <Typography variant="body2" color="text.secondary">
+            {exam.description.substring(0, 100)}...
           </Typography>
         </CardContent>
-      </Collapse>
-    </Card>
-    
+        <CardActions disableSpacing>
+          <Link target={"_blank"} to={exam.guide}>
+            <Button title="Guide">
+              <GpsFixedIcon />
+              Guide
+            </Button>
+          </Link>
+          <Link target={"_blank"} to={exam.sample}>
+            <Button title="Sample">
+              <ContentPasteIcon />
+              Sample
+            </Button>
+          </Link>
+          <Link target={"_blank"} to={exam.practice}>
+            <Button title="Practice">
+              <BadgeIcon />
+              Practice
+            </Button>
+          </Link>
+
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Description:</Typography>
+            <Typography paragraph>{exam.description}</Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
     </div>
   );
 }
